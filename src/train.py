8538943f389efb9ded48a73bef4cad0c3fe3c35e
JesusTafolla <chuy_tafolla@hotmail.com>
2026-09-20 21:21:07 -0600

    Change to random state

--- src/train.py
+++ src/train.py
@@ -12,12 +12,12 @@ y = data.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
-mlflow.set_tracking_uri(uri="https://numerate-kilometer-unframed.ngrok-free.dev/")
+mlflow.set_tracking_uri(uri="https://numerate-kilometer-unframed.ngrok-free.dev")
 mlflow.set_experiment("wine_quality_experiment")
 
 with mlflow.start_run():
     # Train a model
-    random_state = 3
+    random_state = 50
     model = RandomForestClassifier(n_estimators=50, random_state=random_state)
     mlflow.log_param("random_state", random_state)
     model.fit(X_train, y_train)
